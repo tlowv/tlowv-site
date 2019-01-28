4de947e408e664f3953e555dc4dbfa23b6f49ece
jason <jason4974@gmail.com>
2019-01-28 16:21:15 -0500

    added divider in header

--- src/components/AttorneysHeader.vue
+++ src/components/AttorneysHeader.vue
@@ -1,10 +1,9 @@
 <template>
     <v-container fluid fill-height class="attorneys-header" style="max-height: 50vh;">
         <v-layout justify-center align-center column pa-5>
-            <div class="display-3 font-weight-light text-xs-center">Meet Our</div>
-            <div class="display-3 font-weight-light text-xs-center mb-3">Attorneys</div>
+            <div class="display-3 font-weight-light text-xs-center mb-3">Meet Our Attorneys</div>
+            <v-divider></v-divider>
             <div class="display-1 font-weight-light text-xs-center">See why we're best suited to defend you.</div>
-            
         </v-layout>
 
     </v-container>
@@ -17,7 +16,14 @@ export default {
 </script>
 
 <style scoped>
-.attorney-header {
+.attorneys-header {
     background-color: lightgrey;
+}
+
+hr {
+    color: black;
+    width: 90%;
+    margin-bottom: 15px;
+    border-style: solid;
 }
 </style>
