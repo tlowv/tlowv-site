1cc23a2d91f0393497443ee3f4c5f1ecf7086ff5
jason <jason4974@gmail.com>
2019-01-31 17:01:15 -0500

    working on getting contact form to submit properly; all working except capturing the actual data

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -4,33 +4,38 @@
             <div>
                 <h3 class="headline mb-0">Drop us a line</h3>
                 <div>Fill out and submit this form to send us an e-mail</div>
+                
             </div>
         </v-card-title>
         <div class="pa-3">
-            <v-form ref="form" v-model="valid" lazy-validation>
+            <v-form action="https://formspree.io/jparsons@taylorlawofficewv.com" method="POST" ref="form" v-model="valid" lazy-validation>
                 <v-text-field
                     v-model="name"
                     :rules="nameRules"
                     label="Name"
                     required
+                    id="nameInput"
                 ></v-text-field>
                 <v-text-field
                     v-model="email"
                     :rules="emailRules"
                     label="E-mail"
                     required
+                    id="emailInput"
                 ></v-text-field>
+                <div class="ma-0 pa-0 caption grey--text ">We'll never share your email with anyone else.</div>
                 <v-textarea 
                     v-model="message" 
                     :rules="messageRules"
                     required
+                    id="inquiryInput"
                 >
                     <div slot="label">Send us a message</div>
                 </v-textarea>
                 <v-card-actions>
                     <v-spacer></v-spacer>
-                    <v-btn flat round class="white--text red lighten-2" @click="clear">Clear</v-btn>
-                    <v-btn flat round class="white--text green lighten-2" @click="submit">Submit</v-btn>
+                    <v-btn flat round class="white--text red lighten-2" @click="reset">Reset</v-btn>
+                    <v-btn flat round class="white--text green lighten-2" @click="submit" type="submit" value="Send">Submit</v-btn>
                 </v-card-actions>
             </v-form>
         </div>
@@ -59,17 +64,15 @@ export default {
                 this.snackbar = true;
             }
         },
+        reset () {
+            this.$refs.form.reset()
+            this.$refs.form.resetValidation()
+        },
         submit() {
             if (this.validate()) {
                 this.$v.$touch;
             }
         },
-        clear() {
-            this.$v.$reset();
-            this.name = '';
-            this.email = '';
-            this.message = '';
-        }
     }
 };
 </script>
