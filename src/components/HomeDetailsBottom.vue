faf082fbb47a5e0b976f71c36e849030849e34c6
jason <jason4974@gmail.com>
2019-02-18 14:58:46 -0500

    created distinct 'homeDetailsBottom' section to update independently

---
+++ src/components/HomeDetailsBottom.vue
@@ -0,0 +1,49 @@
+<template>   
+  <v-container>
+    <v-layout column>
+      <!-- <v-flex class="display-2 text-xs-center my-5">Why Taylor Law Office is Right for You</v-flex> -->
+        <v-flex>
+          <!-- <div class="headline mt-3">Exceptional Service</div> -->
+          <p class="subheading mt-3">
+            Exceptional service at a reasonable rate with the client's interest always placed first. 
+            Taylor Law Office is committed to this simple principle. In practice, this principle 
+            is exhibited in numerous ways. First, we aggressively defend all cases by tirelessly 
+            investigating and thoroughly developing all viable arguments in our client's favor. 
+            Through discovery, whether written or depositions, our attorneys pursue all angles to 
+            defend our clients. By approaching all cases in this manner, should the case go to trial, 
+            our attorneys are prepared to make the absolute strongest arguments.
+          </p>
+        </v-flex>
+        <v-flex>
+          <!-- <div class="headline mt-3">Reasonable Costs</div> -->
+          <p class="subheading mt-3">
+            In addition to quality service, Taylor Law Office is dedicated to providing such 
+            representation at a reasonable price. Not only are Taylor Law Office's hourly rates 
+            reduced as compared to other firms, but most of the costs and expenses associated with 
+            legal representation are not forced upon the insurer. Among other things, this includes 
+            costs for electronic legal research.
+          </p>     
+        </v-flex>
+        <v-flex>
+          <!-- <div class="headline mt-3">Responsive Action</div> -->
+          <p class="subheading mt-3">
+            Finally, it is the client that is placed at the forefront of all decisions at Taylor 
+            Law Office. From the outset of all cases, our attorneys meet with all clients and 
+            discuss the process involved with civil litigation. This most-basic step develops a 
+            bond with the client that fosters trust and strengthens defenses. In fact, on multiple 
+            occasions, insureds defended by Taylor Law Office attorneys have returned for other 
+            legal needs when issues arise.
+          </p>
+        </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+    name: 'HomeDetailsLower'
+};
+</script>
+
+<style scoped>
+</style>
