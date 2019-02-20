45e8159900f434a1753709a6f148797e3a591eea
jason <jason4974@gmail.com>
2019-02-20 16:59:34 -0500

    playing around in the sandbox

--- src/mixins/titleMixin.js
+++ src/mixins/titleMixin.js
@@ -1,9 +1,7 @@
 function getTitle (vm) {
   const { title } = vm.$options
   if (title) {
-    return typeof title === 'function'
-      ? title.call(vm)
-      : title
+    return typeof title === 'function' ? title.call(vm) : title
   }
 } 
 
--- src/views/Sandbox.vue
+++ src/views/Sandbox.vue
@@ -1,6 +1,10 @@
 <template>
   <span>
-    
+    <div class="container">
+      <div class="item"></div>
+      <div class="item"></div>
+      <div class="item"></div>
+    </div>
   </span>  
 </template>
 
@@ -16,5 +20,25 @@ export default {
 </script>
 
 <style>
+.container {
+  display: flex;
+  height: 500px;
+  width: 100%;
+  justify-content: space-between;
+  
+}
 
+.item {
+  width: 200px;
+}
+
+.item:nth-child(1) {
+  background-color: burlywood;
+}
+.item:nth-child(2) {
+  background-color: teal;
+}
+.item:nth-child(3) {
+  background-color: rosybrown;
+}
 </style>
