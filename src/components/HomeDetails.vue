348a2afbbdff8a1f45dfa3512af51c7adcfa0073
jason <etaylor@taylorlawofficewv.com>
2023-04-02 15:26:33 -0400

    halp

--- src/components/HomeDetails.vue
+++ src/components/HomeDetails.vue
@@ -15,7 +15,7 @@
           Taylor Law Office provides extremely effective service for a very reasonable, 
           competitive price.  Quality defense for a fair price.
         </p>
-        <a class="text-xs-center" href="/contact">Contact us today for a quote.</a>
+        <!-- <a class="text-xs-center" href="/contact">Contact us today for a quote.</a> -->
       </v-flex>
       <v-flex xs12 md4 class="home-block">
         <v-icon size="128" color="amber darken-1">mdi-gavel</v-icon>
@@ -24,7 +24,7 @@
           Whether through trial victories or settlement negotiations, our clients 
           experience a satisfaction rate of over 95%.
         </p>
-        <a class="text-xs-center" href="/attorneys">Learn why our attorneys are so effective.</a>
+        <!-- <a class="text-xs-center" href="/attorneys">Learn why our attorneys are so effective.</a> -->
       </v-flex>
       <v-flex xs12 md4 class="home-block">
         <v-icon size="128" color="amber darken-1">mdi-book-open-page-variant</v-icon>
@@ -33,7 +33,7 @@
           With years of experience across many different practice areas, you can bet 
           that we've seen almost everything.  This experience is priceless.
         </p>
-        <a class="text-xs-center" href="/practice">See which areas we specialize in.</a>
+        <!-- <a class="text-xs-center" href="/practice">See which areas we specialize in.</a> -->
       </v-flex>
     </v-layout>
   </v-container>
